a0f906b550ea8d0a9449959e793f89f26b2cabe2
M <mois20485@gmail.com>
2022-12-11 20:40:58 +0200

    Added Developer and Bug SQL Procedures

---
+++ ProjectManagementSystem/SQLScripts/BugProcedures.sql
@@ -0,0 +1,40 @@
+USE ProjectManagementSystem;
+
+CREATE PROCEDURE ProcedureBugAdd
+(
+    @Title NVARCHAR,
+    @Description NVARCHAR,
+    @Status INT,
+    @Priority INT,
+    @CreatedOn DateTime,
+    @UpdatedOn DateTime,
+    @ProjectId INT
+)
+AS
+    INSERT INTO Bug(Title, Description, Status, Priority, CreatedOn, UpdatedOn, ProjectId)
+    VALUES(@Title, @Description, @Status, @Priority, @CreatedOn, @UpdatedOn, @ProjectId)
+GO;
+
+CREATE PROCEDURE ProcedureBugUpdate
+(
+    @Id INT,
+    @Title NVARCHAR,
+    @Description NVARCHAR,
+    @Status INT,
+    @Priority INT,
+    @UpdatedOn DATETIME
+)
+AS
+    UPDATE Bug
+    SET Id = @Id, Title = @Title, Description = @Description, Status = @Status, Priority = @Priority, UpdatedOn = @UpdatedOn
+    WHERE Id = @Id;
+GO;
+
+CREATE PROCEDURE ProcedureBugDelete
+(
+    @Id INT
+)
+AS
+    DELETE  Bug
+    WHERE Id = @Id;
+GO;
